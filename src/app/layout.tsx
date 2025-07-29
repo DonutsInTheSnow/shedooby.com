2c2d568102d8a71ad8b04c279326dea2e0091f02
Rich <rhudson4966@gmail.com>
2025-07-29 16:31:20 -0400

    add title and metadata to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,13 +57,42 @@
 
 
 
-'use client';
+// 'use client';
+
+// import './globals.css';
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
+// import { CartProvider } from '@/lib/CartContext';
+// import { AuthProvider } from '@/lib/auth';
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <body className="bg-[#d7cdbc]">
+//         <AuthProvider>
+//           <CartProvider>
+//             <Header />
+//             {children}
+//             <Footer />
+//           </CartProvider>
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { CartProvider } from '@/lib/CartContext';
 import { AuthProvider } from '@/lib/auth';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Shedooby - Support for Shedooby iOS App',
+  description: 'Official support and marketing site for the Shedooby iOS app, featuring blog posts, merch, and app tips.',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
