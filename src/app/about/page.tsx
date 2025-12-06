e435a312bca89acfe67cc99f04a8f62eb39d20d3
Rich <rhudson4966@gmail.com>
2025-12-06 11:13:20 -0500

    update next v and About page content tweak

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,10 +10,10 @@ export default function About() {
             <div className="col-start-1 col-end-3 lg:col-end-2">
                 <p className="pb-3">Discipline wins wars—inner wars most of all. Shedooby is your training ground. Your 56-day mission to eliminate distraction, rebuild focus, and reclaim your time.</p>
                 <p className="pb-3">In this country, freedom is earned. Our Constitution guarantees it. The First Amendment gives you a voice. The Second ensures you keep it. But too many Americans are losing one freedom that&apos;s not enshrined: control over their own minds.</p>
-                <p>This app was born out of necessity. I&apos;ve crossed life&apos;s 50-yard line, and the glass now looks half-empty. The time for excuses is over. No more squandering hours arguing with strangers. No more soaking in dopamine traps built to addict. No more chilling out.</p>
+                <p>This app was born out of necessity. The time for excuses is over. No more squandering hours arguing with strangers. No more soaking in dopamine traps built to addict. No more chilling out.</p>
             </div>
             <div className="col-start-1 col-end-3 lg:col-start-2">
-                <p className="pb-3">I built Shedooby as a digital drill sergeant. Every day, several times a day, it calls you to attention. It reminds you of your mission. Stay off social media. Stay sober. Show up with purpose. The weak will fall off. The committed will emerge transformed.</p>
+                <p className="pb-3">Shedooby is your digital drill sergeant. Every day, several times a day, it calls you to attention. It reminds you of your mission. Stay off social media. Stay sober. Show up with purpose. The weaker ones will lay down quick, the stronger will emerge transformed.</p>
                 <p className="pb-3">Forget <em>likes</em>—this is about self-respect. Forget distractions—this is about mental armor. You&apos;ve got 56 days to complete your tour. If you make it to the end, you&apos;ll prove beyond all doubt:</p>
                 <p className="pb-3">Your mind is yours to command.</p>
                 <p>TEN-HUT.</p>
